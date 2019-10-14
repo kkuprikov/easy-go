71977be43f988b80cb52fe28b3e4e31940ff9d5c
Kirill Kuprikov <k_kuprikov@gcore.lu>
2019-10-14 15:44:45 +0300

    Added code formatting via goimports

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/go-redis/redis/v7 v7.0.0-beta.4 // indirect
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/gorilla/websocket v1.4.1
+	github.com/mdempsky/gocode v0.0.0-20190203001940-7fb65232883f // indirect
+	golang.org/x/tools v0.0.0-20191012152004-8de300cfc20a // indirect
 )
--- main.go
+++ main.go
@@ -1,83 +1,89 @@
 package main
 
 import (
-    "github.com/gorilla/websocket"
-    "github.com/gomodule/redigo/redis"
-    "fmt"
-    "strings"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gomodule/redigo/redis"
+	"github.com/gorilla/websocket"
 )
 
 type message struct {
-  id string
+	ID string
 }
 
 const (
-  WS_PORT = ":1234"
+	WS_PORT = ":1234"
 )
 
+var prefix_to_queue = map[string]string{
+	"heatmap": "heatmap_stats",
+	"default": "realtime_stats",
+}
+
 var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
 func newPool() *redis.Pool {
-  return &redis.Pool{
-      MaxIdle:   80,
-      MaxActive: 12000, // max number of connections
-      Dial: func() (redis.Conn, error) {
-          c, err := redis.Dial("tcp", ":6379")
-          if err != nil {
-              panic(err.Error())
-          }
-          return c, err
-      },
-  }
+	return &redis.Pool{
+		MaxIdle:   80,
+		MaxActive: 12000, // max number of connections
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", ":6379")
+			if err != nil {
+				panic(err.Error())
+			}
+			return c, err
+		},
+	}
 }
 
 var pool = newPool()
 
 func main() {
 
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-      upgrader.CheckOrigin = func(r *http.Request) bool {
-        return true
-      }
-      conn, err := upgrader.Upgrade(w, r, nil)
-      if err != nil {
-          log.Println(err)
-          return
-      }
-      go Reader(conn, r.URL.Path)
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		upgrader.CheckOrigin = func(r *http.Request) bool {
+			return true
+		}
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		go Reader(conn, r.URL.Path)
+	})
 
-    if err := http.ListenAndServe(WS_PORT, nil); err != nil {
-        log.Fatal("ListenAndServe:", err)
-    }
+	if err := http.ListenAndServe(WS_PORT, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
 
 func Reader(ws *websocket.Conn, path string) {
-    var conn = pool.Get()
-    for {
-        var msg message
+	var conn = pool.Get()
+	for {
+		var msg message
 
-        if err := ws.ReadJSON(&msg); err != nil {
-          fmt.Println(err)
-          break
-        }
+		if err := ws.ReadJSON(&msg); err != nil {
+			fmt.Println(err)
+			break
+		}
 
-        fmt.Println("Received back from client: " + msg.id)
-        
-        StoreData(path, pool.Get(), msg)
-    }
-    conn.Close()
+		fmt.Println("Received back from client: " + msg.ID)
+
+		StoreData(path, pool.Get(), msg)
+	}
+	conn.Close()
 }
 
 func StoreData(path string, conn redis.Conn, msg message) {
-  parts := strings.Split(path, "/")
-  endpoint := parts[1]
-  jwt := parts[2]
-  fmt.Println(endpoint)
-  fmt.Println(jwt)
+	parts := strings.Split(path, "/")
+	endpoint := parts[1]
+	jwt := parts[2]
+	fmt.Println(endpoint)
+	fmt.Println(jwt)
 }
