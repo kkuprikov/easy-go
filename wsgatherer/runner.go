9d2c1ad7952f062ba2930281eca3b5fd5086d454
Kirill Kuprikov <k_kuprikov@gcore.lu>
2019-10-17 09:37:17 +0300

    Linter errors fix

--- wsgatherer/runner.go
+++ wsgatherer/runner.go
@@ -12,30 +12,38 @@ import (
 )
 
 const (
-	WS_PORT = ":1234"
+	port = ":1234"
 )
 
-var prefix_to_queue = map[string]string{
-	"heatmap": "heatmap_stats",
-	"default": "realtime_stats",
-}
+func queueDict() func(string) string {
+	innerMap := map[string]string{
+		"heatmap": "heatmap_stats",
+		"default": "realtime_stats",
+	}
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+	return func(key string) string {
+		return innerMap[key]
+	}
 }
 
 var pool = NewPool()
 
 func WsHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
 	upgrader.CheckOrigin = func(r *http.Request) bool {
 		return true
 	}
 	conn, err := upgrader.Upgrade(w, r, nil)
+
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
 	go Reader(conn, params.ByName("jwt"))
 }
 
@@ -49,13 +57,14 @@ func Run() {
 
 	router.GET("/ws/send_stat/:jwt", WsHandler)
 
-	if err := http.ListenAndServe(WS_PORT, router); err != nil {
+	if err := http.ListenAndServe(port, router); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
 
 func Reader(ws *websocket.Conn, jwtoken string) {
 	var conn = pool.Get()
+
 	for {
 		var msg map[string]interface{}
 
@@ -74,6 +83,7 @@ func Reader(ws *websocket.Conn, jwtoken string) {
 
 func CombineData(jwtoken string, input map[string]interface{}) map[string]interface{} {
 	var data map[string]interface{}
+
 	inputJSON := ParseJWT(jwtoken)
 
 	if err := json.Unmarshal(inputJSON, &data); err != nil {
@@ -90,24 +100,27 @@ func CombineData(jwtoken string, input map[string]interface{}) map[string]interf
 }
 
 func StoreData(conn redis.Conn, input map[string]interface{}) {
-	var queue_name string
+	var queue string
 
 	if event, ok := input["event"].(string); ok {
-		queue_name = prefix_to_queue[event]
+		queue = queueDict()(event)
 	} else {
 		fmt.Println("Type assertion failed: event is not a string", event)
 	}
 
-	if queue_name == "" {
-		queue_name = prefix_to_queue["default"]
+	if queue == "" {
+		queue = queueDict()("default")
 	}
 
 	msg, _ := json.Marshal(input)
-	conn.Do("LPUSH", queue_name, msg)
+	if _, err := redis.String(conn.Do("LPUSH", queue, msg)); err != nil {
+		fmt.Println("Could not write to redis")
+	}
 
 	// Read for debug
-	res, err := redis.String(conn.Do("LPOP", queue_name))
-	if err == nil {
+	if res, err := redis.String(conn.Do("LPOP", queue)); err != nil {
 		fmt.Println(res)
+	} else {
+		fmt.Println("Could not read from redis")
 	}
 }
