2ff77334961e2bd3ed728cd6794806e8fc32b322
Kirill Kuprikov <k_kuprikov@gcore.lu>
2019-11-01 19:06:26 +0300

    Linter fix

--- wsgatherer/routes.go
+++ wsgatherer/routes.go
@@ -12,12 +12,13 @@ import (
 
 // Start method for our server
 func (s *Server) Start(ctx context.Context, port string, wg *sync.WaitGroup) {
-	s.Router.NotFound = http.FileServer(rice.MustFindBox("static").HTTPBox())
 	s.Router.GET("/ready", s.ready(ctx))
 	s.Router.GET("/spectators", s.spectatorsData())
 
 	s.Router.GET("/ws/send_stat/:jwt", s.statHandler(ctx, wg))
 	s.Router.GET("/ws/subscribe/spectators/:id", s.spectatorHandler(ctx, wg))
+
+	s.Router.NotFound = http.FileServer(rice.MustFindBox("static").HTTPBox())
 
 	if err := http.ListenAndServe(":"+port, s.Router); err != nil {
 		log.Fatal("ListenAndServe:", err)
